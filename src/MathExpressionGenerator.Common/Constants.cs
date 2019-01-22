725f11eacc4c49eed768227d533b1ef75aac4944
Mihail Sotirov <mihail.sotiroff@gmail.com>
2019-01-22 21:35:40 +0200

    Changed the empty box content.

--- src/MathExpressionGenerator.Common/Constants.cs
+++ src/MathExpressionGenerator.Common/Constants.cs
@@ -2,7 +2,7 @@
 {
     public class Constants
     {
-        public const string EmptyBox = "\u0020\u20E3\u0020";//"[  ]";
+        public const string EmptyBox = "[  ]";  //"\u0020\u20E3\u0020";//
         public const int OperandMinValue = int.MinValue;
         public const int OperandMaxValue = int.MaxValue;
         public const int ExpressionsCountMaxValue = 10000;
